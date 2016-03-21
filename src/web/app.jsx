6b8c16e4863c916c998d899ed359b7d343fc87b0
Josh Marinacci <joshua@marinacci.org>
2016-03-20 19:06:38 -0700

    initial import

--- src/web/app.jsx
+++ src/web/app.jsx
@@ -1,4 +1,41 @@
+require('./flexbox.css');
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<button>a button</button>,document.getElementsByTagName("body")[0]);
+class BitmapModel {
+
+}
+
+class DrawingSurface extends React.Component {
+    render() {
+        return <div className="grow">drawing surface</div>
+    }
+}
+
+class LayersPanel extends React.Component {
+    render() {
+        return <ul><li>layer 1</li><li>layer 2</li></ul>
+    }
+}
+
+class Toolbar extends React.Component {
+    render() {
+        return <div className="vbox">
+            <button>color</button>
+            <button>pencil</button>
+            <button>eraser</button>
+            <button>undo</button>
+            <button>redo</button>
+            <button>export</button>
+        </div>
+    }
+}
+
+
+ReactDOM.render(<div className="hbox fill debug">
+    <Toolbar/>
+    <DrawingSurface/>
+    <LayersPanel/>
+</div>,
+    document.getElementsByTagName("body")[0]);
