87f1f4bb1ad5e31152778568eacfe7a2e2d72140
Josh Marinacci <joshua@marinacci.org>
2016-03-20 18:55:45 -0700

    initial import

---
+++ src/web/app.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<button>a button</button>,document.getElementsByTagName("body")[0]);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    context: __dirname + "",
+    entry: {
+        client: "./src/web/app.jsx"
+    },
+    output: {
+        filename: 'index_bundle.js',
+        path: "dist",
+        //publicPath: '/', //set this to override relative paths
+        //hash:true // ??
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title:'List-O-Rama',
+        //template: 'src/web/index.html', //set this to use a template html file
+    })],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+
+            {   test: /\.css$/, loader: "style!css" },
+
+            {   test: /\.ttf$/,   loader: "file?name=[name].[ext]"  },
+            {   test: /\.woff$/,  loader: "file?name=[name].[ext]"  },
+            {   test: /\.woff2$/, loader: "file?name=[name].[ext]"  },
+            {   test: /\.eot$/,   loader: "file?name=[name].[ext]"  },
+            {   test: /\.svg$/,   loader: "file?name=[name].[ext]"  },
+
+        ]
+    }
+};
+
