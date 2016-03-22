e8f61be34cc39967d4af4b004d1ddbb50f7dbb8f
Josh Marinacci <joshua@marinacci.org>
2016-03-22 15:49:51 -0700

    initial stab at a popup with a color picker

--- src/web/app.jsx
+++ src/web/app.jsx
@@ -9,15 +9,77 @@ import ExportPNG from "./ExportPng";
 
 var model = new BitmapModel(16,16);
 
+var PopupState = {
+
+};
+
+class ColorPicker extends React.Component {
+    selectColor(c,e) {
+        console.log("selected color",c);
+        e.stopPropagation();
+    }
+    renderColorWell(c) {
+        return <div key={c}
+                    style={{border:'0px solid black', backgroundColor:c, width:32,height:32, display:'inline-block', margin:0, padding:0}}
+                    onClick={this.selectColor.bind(this,c)}
+        ></div>
+    }
+    render() {
+        var colors = ['red','green','blue','yellow','white','black'];
+        var wells = colors.map(c => this.renderColorWell(c));
+        return <div
+            style={{
+                    margin:0,
+                    padding:0,
+                    display:'flex',
+                    flexDirection:'row',
+                    flexWrap:'wrap',
+                    width:32*3
+            }}
+        >{wells}</div>
+    }
+}
+
+class PopupButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open:false
+        }
+    }
+    componentDidMount() {
+        //PopupState.listen()
+    }
+    clicked() {
+        this.setState({
+            open:true
+        })
+    }
+    render() {
+        return <button style={{ position: 'relative' }} onClick={this.clicked.bind(this)}>
+            {this.props.caption}
+            <div style={{
+                    position: 'absolute',
+                    left:'100%',
+                    top:0,
+                    border: "1px solid #666",
+                    backgroundColor:'white',
+                    padding:'1em',
+                    borderRadius:'0.5em',
+                    display:this.state.open?'block':'none'
+                    }}
+            >{this.props.children}</div>
+        </button>
+    }
+}
 
 class Toolbar extends React.Component {
     exportPNG() {
-        console.log('exporting');
         ExportPNG(model);
     }
     render() {
         return <div className="vbox">
-            <button>color</button>
+            <PopupButton caption="Color"><ColorPicker/></PopupButton>
             <button>pencil</button>
             <button>eraser</button>
             <button>undo</button>
