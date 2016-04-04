d1cedd9be09a4e13767344420b6fe67a6b0bbd03
Josh Marinacci <joshua@marinacci.org>
2016-04-03 21:15:41 -0700

    moved LoginPanel to it's own file rework UserStore to be a proper singleton

--- src/web/app.jsx
+++ src/web/app.jsx
@@ -16,8 +16,8 @@ import LayersPanel from "./LayersPanel.jsx";
 import DocStore from "./DocStore.js";
 import Dialog from "./Dialog.jsx";
 import ExportPNG from "./ExportPng";
-import US from "./UserStore";
-var UserStore = US.init();
+import UserStore from "./UserStore";
+import LoginPanel from "./LoginPanel.jsx"
 
 
 var REQUIRE_AUTH = true;
@@ -139,35 +139,6 @@ class ColorWellButton extends React.Component {
     }
 }
 
-class LoginPanel extends React.Component {
-    tryLogin(e) {
-        e.stopPropagation();
-        var self = this;
-        var data = {
-            username: this.refs.username.value,
-            password: this.refs.password.value
-        };
-        UserStore.login(data, function(user){
-            self.props.onCompleted(user);
-        });
-    }
-    render() {
-        return <div className="body">
-                <div className="hbox">
-                    <label>username</label><input type="text" ref="username"/><br/>
-                </div>
-                <div className="hbox">
-                    <label>password</label><input type="text" ref="password"/><br/>
-                </div>
-                <div className="hbox right">
-                    <button onClick={this.props.onCanceled}>Cancel</button>
-                    <button className="primary" onClick={this.tryLogin.bind(this)}>Login</button>
-                    <label> </label>
-                    <button className="primary" onClick={this.props.switchToRegister}>Register!</button>
-                </div>
-            </div>
-    }
-}
 
 class RegistrationPanel extends React.Component {
     constructor(props) {
