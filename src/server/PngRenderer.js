5ca2183eb5982c4be03f9b6cfa313e7b440d7530
josh <joshua@marinacci.org>
2016-04-20 21:27:13 -0700

    update PNG renderer to handle different image sizes make preview panel support different image sizes added new test image

--- src/server/PngRenderer.js
+++ src/server/PngRenderer.js
@@ -8,9 +8,9 @@ function lookupColor(model,val) {
 }
 
 function drawLayer(c,layer, model, scale) {
-    for(var j=0; j<16; j++) {
-        for(var i=0; i<16; i++) {
-            var index = j*16+i;
+    for(var j=0; j<model.height; j++) {
+        for(var i=0; i<model.width; i++) {
+            var index = j*model.width+i;
             var val = layer.data[index];
             if(val < 0) continue;
             c.fillStyle = lookupColor(model, val);
@@ -29,12 +29,13 @@ function fillRect(c,x,y,w,h) {
 
 module.exports = {
     renderBitmap(model,scale) {
-        var img = PI.make(16*scale,16*scale);
+        console.log(model.width, model.height);
+        var img = PI.make(model.width*scale,model.height*scale);
 
         var c = img.getContext('2d');
         //c.clearRect(0,0,16*scale,16*scale);
         c.fillStyle = "#000000";
-        c.fillRect(0,0,16*scale,16*scale);
+        c.fillRect(0,0,model.width*scale,model.height*scale);
         model.layers.map((layer) => drawLayer(c,layer, model, scale));
         return img;
     }
